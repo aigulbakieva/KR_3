18007a76af1117e73d21bb904a43c5a271a4f89d
aigulbakieva <bakisha.a@mail.ru>
2023-08-19 17:26:48 +0100

    Add get_executed only function

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,17 @@
 import json
+import datetime
 
 
 def load_file():
     with open("operations.json", "r", encoding="UTF-8") as f:
         operations = json.load(f)
         return operations
+
+
+def get_executed(operations):
+    result = []
+    for operation in operations:
+        if 'state' in operation and operation['state'] == 'EXECUTED':
+            result.append(operation)
+    return result
+
