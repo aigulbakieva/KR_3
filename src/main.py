333a4b7a8263c1c2bc4314e67930377ba5d5e548
aigulbakieva <bakisha.a@mail.ru>
2023-08-19 18:31:32 +0100

    Add main func

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from utils import load_file, get_executed, get_sorted_by_date, get_five_operations, prepare_to_output
+
+
+def main():
+    all_operations = load_file()
+    executed_only = get_executed(all_operations)
+    sorted_operations = get_sorted_by_date(executed_only)
+    only_five_operations = get_five_operations(sorted_operations)
+    for operation in only_five_operations:
+        print(prepare_to_output(operation))
+        print()
+
+
+if __name__ == '__main__':
+    main()
